855ee2e2e97c77d463e309025faa936aeef9d274
aswinclimber <ku0260517@gmail.com>
2026-09-01 15:04:58 +0530

    queueUsingArray updated.Done first part of enqueue

--- ads/queue/queueUsingArray.c
+++ ads/queue/queueUsingArray.c
@@ -2,7 +2,12 @@
 
 int front=queue=-1;
 
-void enqueue(){
+void enqueue(int *queue,int size){
+	if(rear == size-1){
+		printf("Overflow");
+	}
+	else{
+
 }
 
 void dequeue(){
@@ -15,5 +20,33 @@ void display(){
 }
 
 int main(){
+	int size;
 	printf("Enter the size of queue : ");
-	scanf("%d",)
+	scanf("%d",&size);
+	int queue[size],option,n=1;
+	while(n){
+		printf("------QueueUsingArray------\n1. Enqueue\n2. dequeue\n3. display\n4. peek\n5. exit\n  : ");
+		scanf("%d",&option);
+		switch(option){
+			case 1:
+				enqueue(queue,size);
+				break;
+			case 2:
+				dequeue();
+				break;
+			case 3:
+				display();
+				break;
+			case 4:
+				peek();
+				break;
+			case 5:
+				n = 0;
+				break;
+			defualt:
+				printf("\nInvalid option");
+			}
+	}
+}
+	
+
