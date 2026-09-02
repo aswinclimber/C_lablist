ca97b7c4fbe77d0be0f8ad6a573e7825abcd3a42
aswinclimber <ku0260517@gmail.com>
2026-09-02 14:10:07 +0530

    apm lablist datacleaningutility finished

--- apm/lablist/datacleaningutility.c
+++ apm/lablist/datacleaningutility.c
@@ -2,12 +2,41 @@
 
 int main(){
 	int n,i,j,k;
+	int same;
 	printf("Enter the number of email address : ");
 	scanf("%d",&n);
 	char emails[n][100];
 	printf("\nEnter the mails : ");
 	for(i=0;i<n;i++){
-		scanf("%s",
+		scanf("%s",emails[i]);
+	}
+
+	for(i=0;i<n;i++){
+		for(j=i+1;j<n;j++){
+			same =1;
+			for(k=0;emails[i][k]!='\0' || emails[j][k]!='\0';k++){
+				if (emails[i][k] != emails[j][k]){
+					same = 0;
+					break;
+				}
+			}
+
+			if(same){
+				for(k=j; k<n-1; k++){
+					for (int x=0;emails[k+1][x] != '\0' || emails [k][x] != '\0'; x++){
+						emails[k][x] = emails[k+1][x];
+					}
+				}
+				n--;
+				j--;
+			}
+		}
+	}
+	printf("\n There");
+	for(i=0; i<n; i++){
+		printf("%s\n",emails[i]);
+	}
+	return 0;
 	
 
 }
