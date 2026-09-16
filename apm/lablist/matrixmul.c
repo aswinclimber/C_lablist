1bd5eb496d28506d349ad86bb9fe9170731c62ca
aswinclimber <ku0260517@gmail.com>
2026-09-16 13:56:24 +0530

    hjhdjhd

---
+++ apm/lablist/matrixmul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int a[100][100];
+	int b[100][100];
+	int r,c;
+
+	printf("Enter the rows and columns of first matrix : ");
+	scanf("%d %d",r,c);
+
+	printf("Enter the elements : ");
+
+
